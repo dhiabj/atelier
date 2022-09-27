e065f464ce1436f8d746d57a485f2f477cf436f0
Dhia Bejaoui <bejaoui.dhia@esprit.tn>
2022-09-27 22:42:47 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
+import { ListuserComponent } from './listuser/listuser.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    FooterComponent,
+    ListuserComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, HeaderComponent, ContentComponent, FooterComponent, ListuserComponent]
 })
 export class AppModule { }
---
+++ src/app/listuser/listuser.component.ts
@@ -0,0 +1,89 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user';
+
+@Component({
+  selector: 'app-listuser',
+  templateUrl: './listuser.component.html',
+  styleUrls: ['./listuser.component.css']
+})
+export class ListuserComponent implements OnInit {
+  list : User[]=[
+    {
+    idCustomer: 1,
+    firstName: "Mila",
+    lastName: " Kunis",
+    birthDate: "1999-06-30",
+    accountCategory: "Admin",
+    email: "mila@kunis.com",
+    password: "test",
+    picture: "https://bootdey.com/img/Content/avatar/avatar3.png",
+    profession: "Software Engineer"
+    },
+    {
+    idCustomer: 2,
+    firstName: "George",
+    lastName: "Clooney",
+    birthDate: "1999-06-30",
+    accountCategory: "Customer",
+    email: "marlon@brando.com",
+    password: "test",
+    picture: "https://bootdey.com/img/Content/avatar/avatar2.png",
+    profession: "Software Engineer"
+    },
+    {
+    idCustomer: 3,
+    firstName: "George",
+    lastName: "Clooney",
+    birthDate: "1999-06-30",
+    accountCategory: "Customer",
+    email: "marlon@brando.com",
+    password: "test",
+    picture: "https://bootdey.com/img/Content/avatar/avatar1.png",
+    profession: "Software Engineer"
+    },
+    {
+    idCustomer: 4,
+    firstName: "Ryan",
+    lastName: "Gossling",
+    birthDate:"1999-06-30",
+    accountCategory: "Golden",
+    email: "Ryan@nicholson.com",
+    password: "test",
+    picture: "https://bootdey.com/img/Content/avatar/avatar4.png",
+    profession: "Software Engineer"
+    },
+    {
+    idCustomer: 5,
+    firstName: "Robert",
+    lastName: "Downey",
+    birthDate: "1999-06-30",
+    accountCategory: "Blocked Account",
+    email: "robert@nicholson.com",
+    password: "test",
+    picture: "https://bootdey.com/img/Content/avatar/avatar5.png",
+    profession: "Software Engineer"
+    }
+    ];
+
+    deleteRow(id:Number){
+      for(let i = 0; i < this.list.length; ++i){
+          if (this.list[i].idCustomer === id) {
+              this.list.splice(i,1);
+          }
+      }
+  }
+
+  searchUsers = (value : any) => {
+    const searchedUsers = this.list.filter((user) => {
+      return user.accountCategory.includes(value);
+    });
+    this.list = searchedUsers;
+    console.log(this.list);
+  };
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
